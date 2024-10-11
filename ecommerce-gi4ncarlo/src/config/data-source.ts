45bbaa3db752d86dd22988664033ec2e594d5804
Giancarlo <giancarlozanarini9@gmail.com>
2024-10-11 19:13:34 -0300

    Arreglo conflictos

--- ecommerce-gi4ncarlo/src/config/data-source.ts
+++ ecommerce-gi4ncarlo/src/config/data-source.ts
@@ -1,56 +1,47 @@
-import { registerAs } from "@nestjs/config";
-import { DataSourceOptions, DataSource } from "typeorm";
-import * as dotenv from "dotenv"
+import { registerAs } from '@nestjs/config';
+import { DataSourceOptions, DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
 
 dotenv.config({
-    path: ".env.development"
-})
+  path: '.env.development',
+});
 
 const SqliteTestDataSourceOptions: DataSourceOptions = {
-    type: "sqlite",
-    database: ":memory:",
-    entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-    synchronize: true,
-    //dropSchema: true
-}
+  type: 'sqlite',
+  database: ':memory:',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
+  //dropSchema: true
+};
 
-const PostgresDataSourceOptions : DataSourceOptions = {
-    type: 'postgres',
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    synchronize: true,
-    logging: true,
-    entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-    migrations:["dist/migration/*{.ts,.js}"],
-    subscribers : [],
-    //dropSchema : true,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    ssl: {
-        rejectUnauthorized: false,
-=======
-       ssl: {
-        rejectUnauthorized: false, // Esto evita errores en desarrollo si no se confía en el certificado
->>>>>>> 8c7e4bb0122f9cdab7b2868c218f904f6d8861de
-    },
-=======
-    //ssl: true
->>>>>>> parent of 13e7e0f (Update data-source.ts SSL)
-}
+const PostgresDataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  synchronize: true,
+  logging: true,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: ['dist/migration/*{.ts,.js}'],
+  subscribers: [],
+  //dropSchema : true,
+  ssl: {
+    rejectUnauthorized: false, // Esto evita errores en desarrollo si no se confía en el certificado
+  },
+};
 
 export const PostgresDataSourceConfig = registerAs(
-    "postgres",
-    ()=> PostgresDataSourceOptions,
+  'postgres',
+  () => PostgresDataSourceOptions,
 );
 
 export const sqliteDataSourceConfig = registerAs(
-    "sqlite",
-    () => SqliteTestDataSourceOptions,
-)
+  'sqlite',
+  () => SqliteTestDataSourceOptions,
+);
 
-export const PostgresDataSource = new DataSource(PostgresDataSourceOptions)
+export const PostgresDataSource = new DataSource(PostgresDataSourceOptions);
 
-export const SqliteDataSource = new DataSource(SqliteTestDataSourceOptions)
+export const SqliteDataSource = new DataSource(SqliteTestDataSourceOptions);
